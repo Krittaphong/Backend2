4e441d5613ed4b784540021b52731def4c1b472a
krittaphong <krittaphong3012@gmail.com>
2024-02-06 11:25:35 +0700

    Done

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -4,7 +4,7 @@ const app = express()
 var bodyParser = require('body-parser')
 
 
-const base_url = "http://localhost:5500"
+const base_url = "http://localhost:3000"
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
