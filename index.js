c9262f0b7060c64ada6703893bd2a14bb699395c
krittaphong <krittaphong3012@gmail.com>
2024-02-13 09:55:36 +0700

    Done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser')
 //const base_url = "http://localhost:3000"
 const base_url = "http://node58308-krittaphong.proen.app.ruk-com.cloud"
 
-app.set('views', path.join(_dirname, "/public/views"));
+//app.set('views', path.join(_dirname, "/public/views"));
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
